1e247c07ea4abebd510c2ad71172e60f127b9581
potential20 <passion212@nate.com>
2020-03-24 10:40:45 +0900

    보너스 번호 생성까지 진행

--- app/src/main/java/kr/co/tjoeun/finalproject_02_lottosimulator/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/finalproject_02_lottosimulator/MainActivity.java
@@ -111,7 +111,27 @@ public class MainActivity extends BaseActivity {
             winNumTxtList.get(i).setText(winLottoNumArr[i]+"");
         }
 
+//            보너스번호 생성 => 1~45, 당첨번호 중복 X.
 
+        while (true) {
+            int randomNum = (int) (Math.random() * 45 + 1);
+
+            boolean isDuplOk = true;
+            for (int winNum : winLottoNumArr) {
+                if (winNum == randomNum){
+                    isDuplOk = false;
+                    break;
+                }
+            }
+
+            if(isDuplOk) {
+                bonusNum = randomNum;
+                break;
+            }
+        }
+
+//        보너스넘버 생성됨.
+        binding.bonusNumTxt.setText(bonusNum+"");
 
     }
 }
